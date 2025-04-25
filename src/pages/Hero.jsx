79d4a9169b2f743fee59911381ae5738f06d94dd
Ray <r24wen@uwaterloo.ca>
2025-04-24 22:01:54 -0400

    Animation works

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -7,44 +7,35 @@ function Hero() {
   return (
     <>
     <div className="heroContainer">
-      <a href="https://github.com/Raybolt-wen" target="_blank" rel="noopener noreferrer">
-      <motion.div
-        // whileHover={{ scale: 1.1 }}
-        // transition={{ type: "tween", stiffness: 300 }}
-        // whileHover={{
-        //   // rotateY: 5,
-        //   // rotateX: -5,
-        //   // scale: 1.02,
-        //   // boxShadow: "0 25px 50px -12px rgba(0, 0, 0, 0.25)"
-        //   backgroundColor: "blue", // Target color
-        //   color: "blue",
-        //   transition: { duration: 0.3 }
-        // }}
-        // transition={{ type: "spring", 
-        //               stiffness: 300,
-        //               damping: 10 }}
-        whileHover={{
-          borderColor: "#0000FF",  // Use HEX
-          backgroundColor: "rgba(0,0,255,0.1)", // Optional background
-          transition: { duration: 0.3 }
-        }}        
-        >
-        <img 
-          src={process.env.PUBLIC_URL + "/photos/Git.png"}
-          alt="GitHub" 
-          style={{ 
-            position: 'absolute',
-            right: 720,
-            width: 200, 
-            height: 180,
-            top: 250, 
-            objectFit: 'cover', 
-            cursor: 'pointer',
-            border: 'solid',
-            borderColor: 'white'}}
-        />
-        </motion.div>
+    <a href="https://github.com/Raybolt-wen" target="_blank" rel="noopener noreferrer">
+          <motion.div
+            style={{
+              position: 'absolute',
+              right: 720,
+              top: 250,
+              border: '2px solid white',
+              borderRadius: '8px',
+              padding: '8px',
+              cursor: 'pointer',
+              borderColor: 'white'
+            }}
+            whileHover={{
+              backgroundColor: "rgba(0, 47, 255, 0.1)",
+              transition: { duration: 0.3 }
+            }}
+          >
+            <img 
+              src={process.env.PUBLIC_URL + "/photos/Git.png"}
+              alt="GitHub" 
+              style={{ 
+                width: 180,
+                height: 150,
+                objectFit: 'cover' 
+              }}
+            />
+          </motion.div>
         </a>
+
         <a href="https://www.linkedin.com/in/ruide-wen-bb1aa022b/" target="_blank" rel="noopener noreferrer">
         <img 
           src={process.env.PUBLIC_URL + "/photos/Linkdin.png"}
